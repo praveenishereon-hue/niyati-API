a1581834408a03416e35ee971287dffa1c2aa0a5
praveenishereon-hue <praveenishereon@gmail.com>
2025-11-06 14:16:32 +0530

    Update niyati.js

--- api/niyati.js
+++ api/niyati.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   // CORS (allow browser calls)
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).json({ error: "POST only" });
 
